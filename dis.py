7296563ac1de86ad52e03fb998f35a4b22d93149
Namylus <follynamylus@gmail.com>
2024-06-03 05:52:49 +0100

    first

--- dis.py
+++ dis.py
@@ -6,8 +6,9 @@ from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 import nltk
-nltk.data.path.append("/opt/render/nltk_data")
 nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
 
 #from imblearn.over_sampling import SMOTE
 tab_2, tab_1,tab_3 = st.tabs(['Diagnose','Information About Diagnosis','Health Information'])
